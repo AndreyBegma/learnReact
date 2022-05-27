fb9e7ac4f8e48b0f9db3bf54d616f2a60592c833
AndreyBegma <abegma137@gmail.com>
2022-05-27 19:40:32 +0300

    create new style for social network

--- react-client/src/components/Header.jsx
+++ react-client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import styles from './Header.module.css'
 
 const Header = () => {
-     let logoCompany = "https://thumbs.dreamstime.com/b/business-company-logo-27438246.jpg"
+     let logoCompany = "http://31.172.65.179:2000/img/logo-icon.png"
      return(<header className={styles.header}>
      <img className={styles.logo_img} src={logoCompany} />
    </header>)
