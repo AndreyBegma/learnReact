db129c763344d6135d4684558bc81a769caecbed
AndreyBegma <abegma137@gmail.com>
2022-05-28 14:21:15 +0300

    Add dialos to component Dialogs

--- react-client/src/components/Dialogs/Dialogs.jsx
+++ react-client/src/components/Dialogs/Dialogs.jsx
@@ -5,8 +5,23 @@ import styles from './Dialogs.module.css'
 const Dialogs = (props) => {
 
      return(
-          <div>
-               Dialogs 
+          <div className={styles.dialogs}>
+               <div className={styles.dialogs_item}>
+                    <div className={styles.dialog + ' ' + styles.active}>
+                         Andrey
+                    </div>
+                    <div className={styles.dialog}>
+                         Maks
+                    </div>
+               </div>
+               <div className={styles.messages}>
+                    <div className={styles.message}>
+                         Hi!
+                    </div>
+                    <div className={styles.message}>
+                         Wtf!?
+                    </div>
+               </div>
           </div>
      )
 }
